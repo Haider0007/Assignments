e4edfee713b1c94d96feaede68026cbc6078488e
Muhammad Haider Iqbal <haider@Muhammads-MacBook-Pro.local>
2019-07-11 17:15:27 +0500

    problem_1

---
+++ problem1.rb
@@ -0,0 +1,9 @@
+
+#Current_time
+time = Time.new
+
+puts time.strftime("Current Date and Time: %d-%m-%y %I:%M %p")
+
+puts time.strftime("Current Date and Time: %d-%m-%y %H:%M")
+
+puts time.strftime("Current Date and Time: %A %d %B %Y %I:%M %p")
