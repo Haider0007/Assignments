c95729eefbd38edcca7f6c74cf3e61b35b0b8edf
Haider0007 <52425491+Haider0007@users.noreply.github.com>
2019-07-11 17:27:51 +0500

    Delete problem1.rb

--- problem1.rb
+++
@@ -1,9 +0,0 @@
-
-#Current_time
-time = Time.new
-
-puts time.strftime("Current Date and Time: %d-%m-%y %I:%M %p")
-
-puts time.strftime("Current Date and Time: %d-%m-%y %H:%M")
-
-puts time.strftime("Current Date and Time: %A %d %B %Y %I:%M %p")
