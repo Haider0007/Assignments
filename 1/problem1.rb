64b0def5d873d36b51551a70bb3bcad899d00968
Muhammad Haider Iqbal <haider@Muhammads-MacBook-Pro.local>
2019-07-12 14:27:16 +0500

    Solution FOr Array/Hash problem 1

---
+++ 1/problem1.rb
@@ -0,0 +1,9 @@
+my_array1=[1,2,3,4,5,6,7]
+my_array2=[7,2,3,4,5,6,10]
+my_array3=[1,2,3,4,5,6,8]
+
+puts my_array1[0] == 7 || my_array1[-1]==7 ? "True" : "False"
+
+puts my_array2[0] == 7 || my_array2[-1]==7 ? "True" : "False"
+
+puts my_array3[0] == 7 || my_array3[-1]==7 ? "True" : "False"
