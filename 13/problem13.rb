70cd517b3be65fcbc9dde7b5cd8902f344a69ced
Muhammad Haider Iqbal <haider@Muhammads-MacBook-Pro.local>
2019-07-12 16:54:27 +0500

    Solution FOr Array/Hash problem 13

---
+++ 13/problem13.rb
@@ -0,0 +1,13 @@
+my_array=[1,[2,3,[4,5,[6]]]]
+
+print "Result:"
+p my_array.flatten(0)
+
+print "Result1:"
+p my_array.flatten(1)
+
+print "Result3:"
+p my_array.flatten(2)
+
+print "Result4:"
+p my_array.flatten(3)
