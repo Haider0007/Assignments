f685c59cab7917a248db43395e18a1628d4154e5
Muhammad Haider Iqbal <haider.iqbal@sendoso.com>
2019-07-13 09:53:16 +0500

    Solution FOr Array/Hash problem 19

---
+++ 19/problem19.rb
@@ -0,0 +1,16 @@
+my_array=[1,2,3,4,5]
+
+print "ORiginal Array:"
+p my_array
+
+print "Reversed Array:"
+p my_array.reverse!
+
+my_new_array=["Haider","Iqbal"]
+
+
+print "ORiginal Array:"
+p my_new_array
+
+print "Reversed Array:"
+p my_new_array.reverse!
