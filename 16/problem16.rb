2f7e5b54c9ef897350a90a2d721e0e51e191c194
Muhammad Haider Iqbal <haider.iqbal@sendoso.com>
2019-07-16 16:43:14 +0500

    Updated Solution for Ruby_Advance_Coding problem16

--- 16/problem16.rb
+++ 16/problem16.rb
@@ -4,12 +4,14 @@ class Person
   def initialize name 
     @name=name
   end
+
+  def method_missing(m, *args, &block)  
+    puts "Hey #{@name}, some_undefined_method is not found in registered methods."  
+  end  
+
 end
 
 
 p=Person.new("Waqas")
-begin
-  p.KuchB
-rescue
-  puts "Hey #{p.name}, some_undefined_method is not found in registered methods."
-end 
+p.KuchB
+
