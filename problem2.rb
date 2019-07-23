64191fe60e0a661dac848d19580200f84e90650f
Muhammad Haider Iqbal <haider.iqbal@sendoso.com>
2019-07-23 21:15:08 +0500

    After Review/Updated Solution for Basic_concepts_assignments_problem_2

--- problem2.rb
+++ problem2.rb
@@ -1,15 +1,15 @@
-puts "Enter Alphabet to print:"
+puts 'Enter Alphabet to print:'
 alphabet=gets.chomp!
 
 valid_num=false
 until valid_num==true
-  puts "Enter number of copies to create:"
+  puts 'Enter number of copies to create:'
   num_copies=Integer(gets.chomp!)
   
   valid_num=num_copies>=0? true : false
-  puts "Wrong Input! Please Enter Again" unless valid_num
+  puts 'Wrong Input! Please Enter Again' unless valid_num
 end  
 
-1.upto(num_copies) do |x|
-  puts alphabet * Integer(x)
+1.upto(num_copies) do |index|
+  puts alphabet * index
 end
