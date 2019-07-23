108dd4dd071164ea5651998db626f51f840930b0
Muhammad Haider Iqbal <haider.iqbal@sendoso.com>
2019-07-23 21:29:17 +0500

    After Review/Updated Solution for Basic_concepts_assignments_problem_3

--- 3/problem3.rb
+++ 3/problem3.rb
@@ -1,4 +1,6 @@
+require 'Date'
+
 puts "Enter year:"
 year=Integer(gets.chomp!)
 
-puts year%4==0 && year%400==0? "Year is leap" : "Year is not leap"
+puts Date.leap?(year)? "Year is leap" : "Year is not leap"
