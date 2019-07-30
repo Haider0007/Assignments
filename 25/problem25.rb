20e42facacccb7a6a7c0dfe15532383c257cf192
Haider0007 <52425491+Haider0007@users.noreply.github.com>
2019-07-30 12:00:11 +0500

    Update problem25.rb

--- 25/problem25.rb
+++ 25/problem25.rb
@@ -4,5 +4,5 @@ str=gets.chomp!
 puts "Enter String to replace with:"
 new_str=gets.chomp!
 
-puts "Updated String:"<<str.sub(str,new_str)
+puts "Updated String:"<<str.gsub(str,new_str)
 puts "Original String:"<<str
